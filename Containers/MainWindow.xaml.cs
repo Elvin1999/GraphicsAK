e78706dc87e46a24fbf40d16f882e1227048af80
camalzadeelvin <camalzadeelvin@STHQ012B-05>
2022-11-12 10:25:13 +0400

    Add project files.

---
+++ Containers/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Containers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DispatcherTimer Timer { get; set; }=new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Timer.Interval = TimeSpan.FromMilliseconds(1);
+            Timer.Tick += Timer_Tick;
+            //Timer.Start();
+
+
+            //stack panel
+            //wrap panel
+            //canvas 
+            //dock panel
+            //uniform grid arashdirma
+            //grid
+        }
+        public int Degree { get; set; } = -90;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Degree += 1;
+            //myLbl.RenderTransform = new RotateTransform
+            //{
+            //    Angle = Degree
+            //};
+        }
+    }
+}
